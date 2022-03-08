3c247c8d0fcba03ba0d6bbab8c04df73cf4ca685
Leon Troper <ltroper@gmail.com>
2022-03-08 15:54:03 -0600

    built backend route

---
+++ backend/routes/api/tracks.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+
+const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { User, Song } = require('../../db/models');
+
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+
+const router = express.Router();
+
+router.get("/:userId", asyncHandler (async (req, res) => {
+    const userId = req.params.userId;
+    const tracks = await Song.findAll({
+        where: {userId}
+    })
+
+    return res.json(tracks)
+}))
+
+module.exports = router
